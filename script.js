d5ee654fe01c9fa04dc8882c626ab3d8c16dccfe
Ahmed Raza <75507078+ahmedraza420@users.noreply.github.com>
2024-03-01 17:27:29 +0500

    Create a function that plays a round of Rock Paper Scissor
    The function plays a single round between the Player and the Computer
    and logs the winner of the round

--- script.js
+++ script.js
@@ -8,5 +8,38 @@ function getComputerChoice () {
 let computerChoice = getComputerChoice();
 let playerChoice = prompt("Choose one:\nRock\nPaper\nScissor");
 
-console.log("Player: " + playerChoice + "\nComputer: " + computerChoice)
+function playRound(playerSelection, computerSelection) {
+    let user = options.indexOf(playerSelection.toLowerCase());
+    let bot = options.indexOf(computerSelection);
+    console.log("player: " + playerSelection + "\ncomputer: " + computerSelection);
 
+
+    let beats = {};
+
+    if (user == 0 && bot == 1)
+    {
+        console.log("You Lose! Paper beats Rock");
+    }
+    else if (user == 1 && bot == 2)
+    {
+        console.log("You Lose! Scissor beats Paper");
+    }
+    else if (user == 2 && bot == 0)
+    {
+        console.log("You Lose! Rock beats Scissor");
+    }
+    else if (user == 1 && bot == 0)
+    {
+        console.log("You Win! Paper beats Rock");
+    }
+    else if (user == 2 && bot == 1)
+    {
+        console.log("You Win! Scissor beats Paper");
+    }
+    else if (user == 0 && bot == 1)
+    {
+        console.log("You Win! Rock beats Scissor");
+    }
+}
+
+playRound(playerChoice, computerChoice);
