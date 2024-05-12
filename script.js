5a799288c676fd21321f3182e0c962961d808496
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-12 14:39:20 +0500

    Fix fetching option index

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 // Display of the Choices
 const playerOptions = document.querySelectorAll(".playerc");
 const computerOptions = document.querySelectorAll(".computerc");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const playerSide = document.querySelectorAll(".choice" && ".playerc");
+const computerSide = document.querySelector("#computerSide");
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
 Array.from(computerOptions).forEach(item => item.style.opacity = "50%");
 
 
 // Game
 let options = ['rock', 'paper', 'scissor'];
+let totalOptions = 3;
 
 function getComputerChoice () {
-    return options[Math.floor(Math.random() * options.length)];
+    return Math.floor(Math.random() * totalOptions);
 }
 
 let computerChoice;
 let playerChoice;
 let roundWinnerScore = [0, 0]; // index 0 for player, index 1 for bot
+playerScore.innerText = roundWinnerScore[0];
+computerScore.innerText = roundWinnerScore[1];
 
-const playerScore = document.querySelector("#playerScore");
-const computerScore = document.querySelector("#computerScore");
+let playerOptionIds = ['playerRock', 'playerPaper', 'playerScissor'];
 
+Array.from(playerSide).forEach(item => {
+item.addEventListener('click', (e) => {
+    computerChoice = getComputerChoice();
+    console.log(computerChoice + " " + playerOptionIds.indexOf(e.target.closest('.choice').getAttribute('id')));
+})
+});
 
+// playerSide.addEventListener('click', (e) => {
+//     computerChoice = getComputerChoice()    
+//     for (index in playerOptionIds) if (playerOptionIds[index] == e.target.closest('.choice').getAttribute('id')) playerChoice = index;
+//     console.log(computerChoice + " " + playerChoice);
+//     startGame();
+// });
 
 
 function playRound(playerSelection, computerSelection) {
@@ -67,7 +87,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-function playGame()
+function startGame()
 {
     let resultString;
     // for (let i = 0; i < 5; i++)
@@ -100,4 +120,3 @@ function playGame()
     }
 }
 
-console.log(playGame());
